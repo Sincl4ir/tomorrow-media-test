82f6a0c42a1bca1b8265a243629b530d08be20fe
Leandro Lopez <llopez.unitydev@gmail.com>
2025-05-09 14:41:59 -0300

    Modified the whole powerup system in order to support concurrent powerups.

--- Assets/QuantumUser/Simulation/Systems/PowerUp/PowerUpSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/PowerUp/PowerUpSystem.cs
@@ -9,46 +9,93 @@ namespace Tomorrow.Quantum
         public unsafe struct Filter
         {
             public EntityRef Entity;
-            public PowerUpHandler* State;
+            public ActivePowerUps* State;
         }
 
         public override void Update(Frame f, ref Filter filter)
         {
-            var def = filter.State;
-            if (!def->IsActive) { return; }
+            if (!filter.State->HasActivePowerUps) { return; }
 
-            def->Timer.Tick(f);
-            if (!def->Timer.IsDone) { return; }
+            var activePowerUps = f.ResolveList(filter.State->ActivePowerUpsList);
+            for (int i = activePowerUps.Count - 1; i >= 0; --i)
+            {
+                var powerUpData = activePowerUps[i];
+                powerUpData.Timer.Tick(f); 
 
-            var powerUp = f.FindAsset(def->PowerUp);
-            if (powerUp == null) { return; }
+                if (!powerUpData.Timer.IsDone)
+                {
+                    activePowerUps[i] = powerUpData;
+                    continue;
+                }
 
-            f.Events.OnPowerUpEnded(filter.Entity, filter.State->PowerUp);
-            powerUp.Remove(f, filter.Entity);
-            def->IsActive = false;
+                var powerUpAsset = f.FindAsset<BasePowerUp>(powerUpData.PowerUp.Id);
+                if (powerUpAsset != null)
+                {
+                    f.Events.OnPowerUpEnded(filter.Entity, powerUpData.PowerUp);
+                    powerUpAsset.Remove(f, filter.Entity);
+                }
+
+                // Remove expired power-up
+                activePowerUps.RemoveAt(i);
+            }
+
+            if (activePowerUps.Count > 0) { return; }
+            filter.State->HasActivePowerUps = false;
+
+            // Copy-pasted from Quantum Docs :)
+
+            // A component HAS TO de-allocate all collection it owns from the frame data, otherwise it will lead to a memory leak.
+            // receives the list QListPtr reference.
+            f.FreeList(filter.State->ActivePowerUpsList);
+
+            // All dynamic collections a component points to HAVE TO be nullified in a component's OnRemoved
+            // EVEN IF is only referencing an external one!
+            // This is to prevent serialization issues that otherwise lead to a desynchronisation.
+            filter.State->ActivePowerUpsList = default;
         }
 
         public void OnPowerUpCollected(Frame f, EntityRef entity, AssetRef<BasePowerUp> powerUpRef)
         {
-            var powerUp = f.FindAsset<BasePowerUp>(powerUpRef.Id);
-            if (powerUp == null)
+            var powerUpAsset = f.FindAsset<BasePowerUp>(powerUpRef.Id);
+            if (powerUpAsset == null)
             {
                 Log.Error($"[PowerUpSystem] Could not find asset {powerUpRef}");
                 return;
             }
 
-            var def = new PowerUpHandler
+            if (!f.Has<ActivePowerUps>(entity))
+            {
+                f.Add(entity, new ActivePowerUps { });
+            }
+
+            var activePowerUps = f.Unsafe.GetPointer<ActivePowerUps>(entity);
+            if (activePowerUps->ActivePowerUpsList == default)
+            {
+                activePowerUps->ActivePowerUpsList = f.AllocateList<PowerUpHandler>();
+            }
+
+            activePowerUps->HasActivePowerUps = true;
+            var powerUpList = f.ResolveList(activePowerUps->ActivePowerUpsList);
+
+            foreach (var handler in powerUpList)
+            {
+                if (handler.PowerUp == powerUpRef)
+                {
+                    Log.Debug("PowerUp already exists, no need to add again");
+                    return;
+                }
+
+            }
+            var powerUpData = new PowerUpHandler
             {
                 PowerUp = powerUpRef,
-                Timer = new Timer(), 
-                IsActive = true
+                Timer = new Timer()
             };
-
-            def.Timer.Start(powerUp.Duration);
-            f.Add(entity, def);
+            powerUpData.Timer.Start(powerUpAsset.Duration);
+            powerUpList.Add(powerUpData);
 
             f.Events.OnPowerUpStarted(entity, powerUpRef);
-            powerUp.Apply(f, entity);
+            powerUpAsset.Apply(f, entity);
         }
     }
 }
